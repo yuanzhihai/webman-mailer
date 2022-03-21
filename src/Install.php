81252a8908c553f887e7fd7321504535a4b42082
yuanzhihai <396751927@qq.com>
2022-03-21 17:02:08 +0800

    init

--- src/Install.php
+++ src/Install.php
@@ -39,13 +39,14 @@ class Install
     {
         foreach (static::$pathRelation as $source => $dest) {
             if ($pos = strrpos($dest, '/')) {
-                $parent_dir = base_path() . '/' . substr($dest, 0, $pos);
+                $parent_dir = base_path().'/'.substr($dest, 0, $pos);
                 if (!is_dir($parent_dir)) {
                     mkdir($parent_dir, 0777, true);
                 }
             }
             //symlink(__DIR__ . "/$source", base_path()."/$dest");
-            copy_dir(__DIR__ . "/Install.php", base_path() . "/$dest");
+            copy_dir(__DIR__ . "/$source", base_path()."/$dest");
+            echo "Create $dest";
         }
     }
 
@@ -56,13 +57,15 @@ class Install
     public static function uninstallByRelation()
     {
         foreach (static::$pathRelation as $source => $dest) {
-            $path = base_path() . "/$dest";
+            $path = base_path()."/$dest";
             if (!is_dir($path) && !is_file($path)) {
                 continue;
             }
-            /*if (is_link($path) {
+            echo "Remove $dest";
+            if (is_file($path) || is_link($path)) {
                 unlink($path);
-            }*/
+                continue;
+            }
             remove_dir($path);
         }
     }
